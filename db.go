6489f9920eea56603a704715fc212d848e8775ca
AriTheFirst <ari@arithefirst.com>
2024-08-25 05:27:08 -0400

    Added get() function for querying the db

--- db.go
+++ db.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type mongoServer struct {
 	Host       string
 	Port       uint16
@@ -13,4 +23,49 @@ type user struct {
 	TC  string `bson:"tc"`
 	STC string `bson:"stc"`
 	BG  string `bson:"bg"`
+}
+
+func get(uid string) (*user, error) {
+	serverData := mongoServer{
+		Host:       "",
+		Port:       0,
+		DB:         "config",
+		Collection: "users",
+	}
+
+	ctx := context.TODO()
+
+	// Set the mongoDB server location
+	client, err := mongo.NewClient(options.Client().ApplyURI(serverData.Host + string(serverData.Port)))
+	if err != nil {
+		panic(err)
+	}
+
+	// Connect to the server
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// Defer the disconnect to the end of the function
+	defer client.Disconnect(ctx)
+
+	// Set the db and collection as well as the search query
+	coll := client.Database(serverData.DB).Collection(serverData.Collection)
+	filter := bson.D{{Key: "uid", Value: uid}}
+
+	// Find one document matching the input uid
+	var output user
+	err = coll.FindOne(ctx, filter).Decode(&output)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			fmt.Println("No documents found with the given name")
+			return nil, nil
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return &output, nil
 }
