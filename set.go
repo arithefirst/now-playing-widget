c3c1fdb67690c0b46170a59de12a78601cc3468a
AriTheFirst <ari@arithefirst.com>
2024-08-27 21:52:55 -0400

    Added security to /api/v1/set to prevent people changing other user's configs

--- set.go
+++ set.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 func sendJsonSet(upsert int64, mod int64, err string) string {
@@ -15,34 +18,89 @@ func sendJsonSet(upsert int64, mod int64, err string) string {
 	}
 }
 
+func authCheck(auth string) string {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodGet, "https://api.spotify.com/v1/me", nil)
+	if err != nil {
+		return err.Error()
+	}
+	req.Header.Set("Authorization", auth)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err.Error()
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err.Error()
+	}
+
+	// Convert the json body into a map
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(body), &result)
+	if err != nil {
+		return err.Error()
+	}
+
+	_, errorExists := result["error"]
+	if errorExists {
+		return string(body)
+	} else {
+		id, ok := result["id"].(string)
+		if !ok {
+			return "Failed to parse json value"
+		} else {
+			return id
+		}
+	}
+
+}
+
 func setConf(w http.ResponseWriter, r *http.Request) {
+	// Set the content-type header to json
+	w.Header().Set("content-type", "application/json; charset=utf-8")
+
 	// Check to see if bg, tc, stc, right, and uid are present in the querystring
 	if r.URL.Query().Get("bg") == "" ||
 		r.URL.Query().Get("tc") == "" ||
 		r.URL.Query().Get("stc") == "" ||
 		r.URL.Query().Get("right") == "" ||
 		r.URL.Query().Get("uid") == "" {
-		fmt.Fprint(w, "All inputs are required to use this endpoint")
+		fmt.Fprint(w, "{\"err\":\"All inputs are required to use this endpoint\"}")
 	} else {
-		var alignRight bool = false
-		if r.URL.Query().Get("right") == "true" {
-			alignRight = true
-		}
-
-		updateParams := user{
-			BG:    r.URL.Query().Get("bg"),
-			TC:    r.URL.Query().Get("tc"),
-			STC:   r.URL.Query().Get("stc"),
-			RIGHT: alignRight,
-			UID:   r.URL.Query().Get("uid"),
-		}
-
-		result, err := set(updateParams)
-		w.Header().Set("content-type", "application/json; charset=utf-8")
-		if err != nil {
-			fmt.Fprint(w, sendJsonSet(0, 0, err.Error()))
+		auth := r.Header.Get("Authorization")
+		if r.URL.Query().Get("uid") != authCheck(auth) {
+			if strings.Contains(authCheck(auth), "No token provided") {
+				fmt.Fprint(w, "{\"err\":\"No Token Provided\"}")
+			} else if strings.Contains(authCheck(auth), "The access token expired") {
+				fmt.Fprint(w, "{\"err\":\"The access token expired\"}")
+			} else if strings.Contains(authCheck(auth), "Only valid bearer authentication supported") {
+				fmt.Fprint(w, "{\"err\":\"Only valid bearer authentication supported\"}")
+			} else {
+				fmt.Fprint(w, "{\"err\":\"You cannot modify another user's configuration\"}")
+			}
 		} else {
-			fmt.Fprint(w, sendJsonSet(result.UpsertedCount, result.ModifiedCount, "null"))
+			var alignRight bool = false
+			if r.URL.Query().Get("right") == "true" {
+				alignRight = true
+			}
+
+			updateParams := user{
+				BG:    r.URL.Query().Get("bg"),
+				TC:    r.URL.Query().Get("tc"),
+				STC:   r.URL.Query().Get("stc"),
+				RIGHT: alignRight,
+				UID:   r.URL.Query().Get("uid"),
+			}
+
+			result, err := set(updateParams)
+			if err != nil {
+				fmt.Fprint(w, sendJsonSet(0, 0, err.Error()))
+			} else {
+				fmt.Fprint(w, sendJsonSet(result.UpsertedCount, result.ModifiedCount, "null"))
+			}
 		}
 	}
 
